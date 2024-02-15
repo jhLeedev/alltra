f28c91014a0434e6b4e36452398a77e78301a5ef
JeongHyeon <jhlee9638@gmail.com>
2024-02-16 03:52:34 +0900

    dotenv import 수정

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { RecoilRoot } from 'recoil';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
