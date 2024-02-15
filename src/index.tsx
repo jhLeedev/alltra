bbb66bf1218d389d48911cd1e119a3a5d07f8bd8
JeongHyeon <jhlee9638@gmail.com>
2024-02-16 04:08:11 +0900

    dotenv import 제거

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { RecoilRoot } from 'recoil';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
